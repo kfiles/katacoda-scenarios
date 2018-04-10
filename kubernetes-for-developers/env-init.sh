24212c127ddd677de95ac9de9ac88d86e7ba7533
Kirby Files <kfiles@masergy.com>
2018-04-10 19:33:19 -0400

    Update init

--- kubernetes-for-developers/env-init.sh
+++ kubernetes-for-developers/env-init.sh
@@ -1,2 +1,2 @@
 ssh root@host01 "while [ \`docker ps | wc -l\` -eq 1 ]; do sleep 1; done && kubectl run kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1 --port=8080 --replicas=4 && kubectl expose deployments/kubernetes-bootcamp --type=\"NodePort\" --port 8080 >> /opt/katacoda-completed"
-ssh root@host01 "kubectl create ns dev && kubectl create ns test && /usr/bin/get_helm.sh && helm init && kubectl completion bash > bash_completion && helm completion bash >> bash_completion && echo done >> /opt/katacoda-completed"
+#ssh root@host01 "kubectl create ns dev && kubectl create ns test && /usr/bin/get_helm.sh && helm init && kubectl completion bash > bash_completion && helm completion bash >> bash_completion && echo done >> /opt/katacoda-completed"
